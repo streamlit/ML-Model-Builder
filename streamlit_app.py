a9347550c9860e183997b4e3ca77b2463846709a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-08 14:19:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-
+import altair as alt
 
 with st.status("Building model...", expanded=True) as status:
     # Loading data
@@ -18,6 +18,11 @@ with st.status("Building model...", expanded=True) as status:
     # Splitting data
     st.write("Splitting data...")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+
+    # Evaluating performance metrics
+    st.write("Evaluating performance metrics")
+    lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
+    lr_train_r2 = r2_score(y_train, y_lr_train_pred)
 
     
     status.update(label="Model built!", state="complete", expanded=False)
