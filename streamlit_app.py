35805abde37e28c872a2e60750dd3d62b07e3c82
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-08 11:44:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
-st.title('🎈 App Name')
+# Load data
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
 
-st.write('Hello world!')
+# Data preparation
+X = df.drop('logS', axis=1)
+y = df['logS']
+
+# Data splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+
+# Linear regression
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+# Applying the model to make a prediction
+y_lr_train_pred = lr.predict(X_train)
+y_lr_test_pred = lr.predict(X_test)
