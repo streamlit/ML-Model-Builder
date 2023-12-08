aeae94db365500a3c03f55ebb2599cc22b48be08
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-08 15:07:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,32 +5,32 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import altair as alt
 
-with st.status("Building model...", expanded=True) as status:
-    st.write("Loading data...")
+with st.status("Building model ...", expanded=True) as status:
+    st.write("Loading data ...")
     df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
     
-    st.write("Preparing data")
+    st.write("Preparing data ...")
     X = df.drop('logS', axis=1)
     y = df['logS']
     
-    st.write("Splitting data...")
+    st.write("Splitting data ...")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
 
-    st.write("Training the model...")
+    st.write("Training the model ...")
     lr = LinearRegression()
     lr.fit(X_train, y_train)
 
-    st.write("Applying model to make predictions...")
+    st.write("Applying model to make predictions ...")
     y_lr_train_pred = lr.predict(X_train)
     y_lr_test_pred = lr.predict(X_test)
     
-    st.write("Evaluating performance metrics...")
+    st.write("Evaluating performance metrics ...")
     lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
     lr_train_r2 = r2_score(y_train, y_lr_train_pred)
     lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
     lr_test_r2 = r2_score(y_test, y_lr_test_pred)
 
-    st.write("Displaying performance metrics")
+    st.write("Displaying performance metrics ...")
     lr_results = pd.DataFrame(['Linear regression', lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
     lr_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
     
