7954fb08ed9a188449b27d2c7d6915b577b0fb93
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-10 20:38:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ with st.status("Running ...", expanded=True) as status:
         for col in rf_results.columns:
             rf_results[col] = pd.to_numeric(rf_results[col], errors='ignore')
         # Round to 3 digits
-        rf_results.round(3)
+        rf_results = rf_results.round(3)
     
     else:
         st.warning('👈 Upload a CSV file to get started!')
