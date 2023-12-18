d8960d249044ae82ee68efb4e6ba70a194cc87fb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-18 20:03:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ with st.sidebar:
 
     # Load data
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
+        df = pd.read_csv(uploaded_file, index_col=False)
     if example_data:
         df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
 
