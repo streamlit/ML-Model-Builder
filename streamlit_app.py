1f35edb03c21adf65824d4d1a1e1712f9f1e0f52
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-18 19:29:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,9 +49,6 @@ with st.sidebar:
     with st.expander('See parameters'):
         parameter_n_estimators = st.slider('Number of estimators (n_estimators)', 0, 1000, 100, 100)
         parameter_max_features = st.select_slider('Max features (max_features)', options=['all', 'sqrt', 'log2'])
-        if parameter_max_features == 'all':
-            parameter_max_features = None
-            parameter_max_features_metric = df.shape[1]-1
         parameter_min_samples_split = st.slider('Minimum number of samples required to split an internal node (min_samples_split)', 2, 10, 2, 1)
         parameter_min_samples_leaf = st.slider('Minimum number of samples required to be at a leaf node (min_samples_leaf)', 1, 10, 2, 1)
 
@@ -82,6 +79,11 @@ if uploaded_file or example_data:
     
         st.write("Training the model ...")
         time.sleep(sleep_time)
+
+        if parameter_max_features == 'all':
+            parameter_max_features = None
+            parameter_max_features_metric = df.shape[1]-1
+        
         rf = RandomForestRegressor(
                 n_estimators=parameter_n_estimators,
                 max_features=parameter_max_features,
