3b44d9ba265a991842746c81f33bf78d94e0f3ef
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-18 19:49:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,10 @@ with st.sidebar:
     st.subheader('2.1. Learning Parameters')
     with st.expander('See parameters'):
         parameter_n_estimators = st.slider('Number of estimators (n_estimators)', 0, 1000, 100, 100)
-        parameter_max_features = st.select_slider('Max features (max_features)', options=['all', 'sqrt', 'log2'])
+        
+        n_features = df.shape[1]-1
+        parameter_max_features = st.slider('Max features (max_features)', 1, n_features, (1, n_features), 1)
+        
         parameter_min_samples_split = st.slider('Minimum number of samples required to split an internal node (min_samples_split)', 2, 10, 2, 1)
         parameter_min_samples_leaf = st.slider('Minimum number of samples required to be at a leaf node (min_samples_leaf)', 1, 10, 2, 1)
 
