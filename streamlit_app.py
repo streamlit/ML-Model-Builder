2c258f1bc98bb518fc30a191cf11af3f8029489b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-21 11:47:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -224,7 +224,7 @@ if uploaded_file or example_data:
     
     # Display dataframe
     with prediction_col[0]:
-        st.dataframe(df_prediction)
+        st.dataframe(df_prediction, use_container_width=True)
 
     # Display scatter plot of actual vs predicted values
     with prediction_col[1]:
